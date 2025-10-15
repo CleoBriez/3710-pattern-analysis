4b689d24b4675002183cbd021e066cf30999a058
CleoBriez <cleodakoda@gmail.com>
2025-10-15 17:08:12 +1000

    init folders

---
+++ recognition/stuff/modules.py
@@ -0,0 +1,7 @@
+# Containing the source code of the components of your model. Each component must be implementated as a class or a function 
+
+def main(): 
+    pass
+
+if __name__ == "__main__":
+    main()
