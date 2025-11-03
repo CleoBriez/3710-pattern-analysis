23d6bef41c723ee9734c82fe1548f8beee1f1b81
CleoBriez <cleodakoda@gmail.com>
2025-11-03 11:09:02 +1000

    added data locally for testing

--- recognition/43711451-HipMRI2D-ImprovedUNET/predict.py
+++ recognition/43711451-HipMRI2D-ImprovedUNET/predict.py
@@ -1,13 +1,17 @@
 # recognition\43711451-HipMRI3D-ImprovedUNET\predict.py
 """
-
+Contains the main prediction script for the model after training
 """
 
-import os
+import dataset as data
+import modules as module
+import torch
+import numpy as np
+from matplotlib import pyplot
 
 __author__ = "Cleodora Kizmann"
 __copyright__ = "Copyright 2025, Cleodora Kizmann"
-__credits__ = ["Cleodora Kizmann, "]
+__credits__ = ["Cleodora Kizmann"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
 __maintainer__ = "Cleodora Kizmann"
